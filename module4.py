e4020683645a5a0e63aa4e94be320062f3488168
sunshine earth <tliu222@my.trine.edu>
2025-05-02 18:58:31 -0700

    Add files via upload

---
+++ module4.py
@@ -0,0 +1,12 @@
+count = int(input("Enter N: "))
+entries = []
+for i in range(count):
+    entry = int(input(f"Enter number {i+1}: "))
+    entries.append(entry)
+
+target = int(input("Enter X: "))
+
+if target in entries:
+    print(entries.index(target) + 1)  # Return 1-based index
+else:
+    print(-1)
